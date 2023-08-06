a34e243b6b3f0f6fe6bd7b77e1a314c1ccfdf750
ABojo <Alex.bojorquez2598@gmail.com>
2023-08-06 16:19:01 -0400

    added breakpoint settings to reduce slide numbers within the slick config. Added padding to the slick slides at lower break points. Increased line height after the second breakpoint

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
-  $(".testimonials__cards").slick({ dots: true, infinite: true, slidesToShow: 3, slidesToScroll: 1 });
+  $(".testimonials__cards").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1070,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 870,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
 });
